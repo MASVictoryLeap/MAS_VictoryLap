781d29dedd4a86a4af9dd6d9157f6e669b291adc
djdangie@yahoo.com <djdangie@yahoo.com>
2014-02-26 23:24:18 -0500

    testing2

--- src/com/example/mas_victoryleap/MainActivity.java
+++ src/com/example/mas_victoryleap/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.mas_victoryleap;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener { 
 
-		//testing
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,6 +20,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
